bbdd1da6b0a5db3f7c6abf63676a7ac32eb6140e
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2023-06-11 02:53:32 -0400

    Renamed file ArrayTools.kt

--- ArrayTools.kt
+++ ArrayTools.kt
@@ -1,24 +1,24 @@
-//Omitting fun main gives main.jar error
-fun main(){
+// Omitting fun main gives main.jar error
+fun main() {
     averageArrayTester()
-    print(averageArray(arrayOf(1,2,3)))
+    print(arrayContains(arrayOf(1, 2, 3), 2)) // Boolean TEST
 }
 // Averaging the integer Array
 // Double gives output in decimals
 
 fun averageArray(inputArray: Array<Int>): Double {
     // To not divide by 0 for output
-    if (inputArray.isEmpty()){
+    if (inputArray.isEmpty()) {
         return 0.0
     }
-    
-       var sum = 0.0
+
+    var sum = 0.0
     for (number in inputArray) {
         sum += number
     }
     return sum / inputArray.size
 }
-//Testing the method
+// Testing the method
 fun averageArrayTester() {
     println(averageArray(arrayOf(1, 2, 3)))
     println(averageArray(arrayOf(-1, -2, -3)))
@@ -27,4 +27,15 @@ fun averageArrayTester() {
     println(averageArray(arrayOf()))
 }
 
+// arrayContains for array and search value
+
+fun arrayContains(inputArray: Array<Int>, target: Int): Boolean {
+
+    for (number in inputArray) {
+        if (number == target) {
+            return true
+        }
+    }
+        return false
+    }
 
