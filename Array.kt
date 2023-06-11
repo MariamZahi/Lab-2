39a76dcc4b188cad740c6656d14c30a62d80bb0a
Mariam Zahid <120362767+MariamZahi@users.noreply.github.com>
2023-06-11 02:00:11 -0400

    Introduced Tester

--- Array.kt
+++ Array.kt
@@ -1,15 +1,35 @@
-//Test the method
 fun main(){
-    print(averageArray(arrayOf(1,2,3,4)))
+    averageArrayTester()
+    print(arrayContains(arrayOf(1,2,3),2))
 }
+// Averaging the integer Array
+// Double gives output in decimals
 
-//Averaging the integer Array
-//Double gives output as decimals 
-
-fun averageArray(inputArray: Array<Int>):Double {
-     var sum = 0.0
-    for (number in inputArray){
+fun averageArray(inputArray: Array<Int>): Double {
+    // To not divide by 0 for output
+    if (inputArray.isEmpty()){
+        return 0.0
+    }
+    
+       var sum = 0.0
+    for (number in inputArray) {
         sum += number
     }
-    return sum/inputArray.size
+    return sum / inputArray.size
+}
+//Testing the method
+fun averageArrayTester() {
+    print(averageArray(arrayOf(1, 2, 3)))
+    print(averageArray(arrayOf(-1, -2, -3)))
+    print(averageArray(arrayOf(-1605)))
+    print(averageArray(arrayOf(99, 10)))
+}
+
+fun arrayContains(inputArray: Array<Int>, target: Int): Boolean {
+    for (element in inputArray) {
+        if (element == target) {
+            return true
+        }
+    }
+    return false
 }
